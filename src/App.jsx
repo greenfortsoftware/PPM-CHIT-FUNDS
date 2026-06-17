d689a44bf19709ff813699789e24e5f4e06a3e4b
KutraleeswaranC <Kutraleeswaranc@gmail.com>
2026-06-17 16:30:19 +0530

    initialization the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './navbar/Navbar'
+import Main from './Main/Main'
+import Footer from './footer/Footer'
+import About from './about/About'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Main/>
+    <About/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
